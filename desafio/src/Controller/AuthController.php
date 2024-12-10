fb127b66c2915757be1146cf122f77ff50a84e99
daniel <danielfsilva.2019.2@gmail.com>
2024-12-09 22:00:01 -0300

    Criação da página de login

---
+++ desafio/src/Controller/AuthController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AuthController extends AbstractController
+{
+    #[Route('/login', name: 'login')]
+    public function login(Request $request): Response
+    {
+        return $this->render('auth/login.html.twig');
+    }
+}
