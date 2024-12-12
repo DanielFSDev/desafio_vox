2eba3f9fa7c423f728ead0396a63ef5c0a15c1bd
daniel <danielfsilva.2019.2@gmail.com>
2024-12-12 08:00:12 -0300

    Criação da rota de registrar usuário

--- desafio/src/Controller/AuthController.php
+++ desafio/src/Controller/AuthController.php
@@ -2,22 +2,43 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\User;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Doctrine\ORM\EntityManagerInterface;
 
 class AuthController extends AbstractController
 {
-    #[Route('/login', name: 'login')]
-    public function login(Request $request): Response
-    {
-        return $this->render('auth/login.html.twig');
-    }
-
-    #[Route('/register', name: 'register')]
-    public function register(Request $request): Response
-    {
-        return $this->render('auth/register.html.twig');
+    #[Route('/register', name: 'register', methods: ['POST'])]
+    public function register(
+        Request $request,
+        UserPasswordHasherInterface $passwordHasher,
+        EntityManagerInterface $entityManager
+    ): JsonResponse {
+        $data = json_decode($request->getContent(), true);
+        if (empty($data['username']) || empty($data['email']) || empty($data['password'])) {
+            return new JsonResponse(['error' => 'Você não preencheu todos os campos.'], Response::HTTP_BAD_REQUEST);
+        }
+        $existingUser = $entityManager->getRepository(User::class)->findOneBy(['username' => $data['username']]);
+        if ($existingUser) {
+            return new JsonResponse(['error' => 'Já existe alguém com esse nome de usuário'], Response::HTTP_CONFLICT);
+        }
+        try {
+            $user = new User();
+            $user->setUsername($data['username']);
+            $user->setEmail($data['email']);
+            $user->setRoles(['ROLE_USER']);
+            $hashedPassword = $passwordHasher->hashPassword($user, $data['password']);
+            $user->setPassword($hashedPassword);
+            $entityManager->persist($user);
+            $entityManager->flush();
+            return new JsonResponse(['message' => 'User created successfully'], Response::HTTP_CREATED);
+        } catch (\Exception $e) {
+            return new JsonResponse(['error' => $e->getMessage()], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
     }
 }
