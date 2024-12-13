82d464cd0d2c205f87fb9e486ad79890d44ec3e8
daniel <danielfsilva.2019.2@gmail.com>
2024-12-12 23:40:51 -0300

    Criar rota de salvar empresa

--- desafio/src/Repository/CompanyRepository.php
+++ desafio/src/Repository/CompanyRepository.php
@@ -2,7 +2,39 @@
 
 namespace App\Repository;
 
-class CompanyRepository
-{
+use App\Entity\Company;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
 
+/**
+ * @method Company|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Company|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Company[]    findAll()
+ * @method Company[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class CompanyRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Company::class);
+    }
+
+    public function findByName(string $name)
+    {
+        return $this->createQueryBuilder('c')
+            ->andWhere('c.name LIKE :name')
+            ->setParameter('name', '%' . $name . '%')
+            ->orderBy('c.name', 'ASC')
+            ->getQuery()
+            ->getResult();
+    }
+
+    public function findByOwner($ownerId)
+    {
+        return $this->createQueryBuilder('c')
+            ->andWhere('c.owner = :ownerId')
+            ->setParameter('ownerId', $ownerId)
+            ->getQuery()
+            ->getResult();
+    }
 }
