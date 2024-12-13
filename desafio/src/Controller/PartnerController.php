b9795c098f97c2007e32caa8b08059177f10cc65
daniel <danielfsilva.2019.2@gmail.com>
2024-12-12 23:28:53 -0300

    Criar rota de adicionar sócio na empresa

--- desafio/src/Controller/PartnerController.php
+++ desafio/src/Controller/PartnerController.php
@@ -3,9 +3,12 @@
 namespace App\Controller;
 
 use App\Entity\Company;
+use App\Entity\CompanyUser;
+use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -29,5 +32,39 @@ class PartnerController extends AbstractController
                 ])->toArray()
             ], Response::HTTP_OK
         );
+    }
+
+    #[Route('/empresa/{id}/partner', methods: ['POST'])]
+    public function addPartner(
+        int $id,
+        Request $request,
+        EntityManagerInterface $entityManager
+    ): JsonResponse {
+        $company = $entityManager->getRepository(Company::class)->find($id);
+        if (!$company) {
+            throw $this->createNotFoundException('Empresa não encontrada.');
+        }
+        $data = json_decode($request->getContent(), true);
+        $codUser = $data['codUser'];
+        $user = $entityManager->getRepository(User::class)->find($codUser);
+        if (!$user) {
+            throw $this->createNotFoundException('Não foi encontrado esse usuário.');
+        }
+        $partnersCompany = $company->getPartners();
+        foreach ($partnersCompany as $companyPartner) {
+            if ($companyPartner->getId() === $user->getId()) {
+                throw $this->createNotFoundException('Sócio já está vínculado com a empresa.');
+            }
+        }
+        if ($company->getOwner()->getId() === $user->getId()) {
+            throw $this->createNotFoundException('Esse sócio é o dono da empresa.');
+        }
+        $companyUser = new CompanyUser();
+        $companyUser->setUser($user);
+        $companyUser->setCompany($company);
+        $companyUser->setRole('Sócio');
+        $entityManager->persist($companyUser);
+        $entityManager->flush();
+        return new JsonResponse(['Sócio adicionado com sucesso: ', $user->getId()], Response::HTTP_OK);
     }
 }
