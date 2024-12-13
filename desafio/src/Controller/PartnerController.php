97880bfbde5264927f3dae5c6c798c0a39192a6e
daniel <danielfsilva.2019.2@gmail.com>
2024-12-12 23:27:22 -0300

    Criar rota de consultar sócios de uma empresa

--- desafio/src/Controller/PartnerController.php
+++ desafio/src/Controller/PartnerController.php
@@ -3,18 +3,16 @@
 namespace App\Controller;
 
 use App\Entity\Company;
-use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 
-class ParternerController extends AbstractController
+class PartnerController extends AbstractController
 {
-    #[Route('/empresa/{id}/parterner', methods: ['GET'])]
-    public function getParterners(
+    #[Route('/empresa/{id}/partner', methods: ['GET'])]
+    public function getPartners(
         int $id,
         EntityManagerInterface $entityManager
     ): JsonResponse {
@@ -22,36 +20,14 @@ class ParternerController extends AbstractController
         if (!$company) {
             throw $this->createNotFoundException('Empresa não encontrada.');
         }
-        return new JsonResponse(['parterners' => $company->getPartners()->toArray()], Response::HTTP_OK);
-    }
-
-    #[Route('/empresa/{id}/parterner', methods: ['POST'])]
-    public function addParterner(
-        int $id,
-        Request $request,
-        EntityManagerInterface $entityManager
-    ): JsonResponse {
-        $company = $entityManager->getRepository(Company::class)->find($id);
-        if (!$company) {
-            throw $this->createNotFoundException('Empresa não encontrada.');
-        }
-        $data = json_decode($request->getContent(), true);
-        $codPartner = $data['codPartner'];
-        $partner = $entityManager->getRepository(User::class)->find($codPartner);
-        if (!$partner) {
-            throw $this->createNotFoundException('Não foi encontrado esse usuário.');
-        }
-        $partnersCompany = $company->getPartners();
-        foreach ($partnersCompany as $companyPartner) {
-            if ($companyPartner->getId() === $partner->getId()) {
-                throw $this->createNotFoundException('Sócio já está vínculado com a empresa.');
-            }
-        }
-        if ($company->getOwner()->getId() === $partner->getId()) {
-            throw $this->createNotFoundException('Esse sócio é o dono da empresa.');
-        }
-        $company->addPartner($partner);
-        $entityManager->flush();
-        return new JsonResponse(['Sócio adicionado com sucesso: ', $partner->getId()], Response::HTTP_OK);
+        return new JsonResponse(
+            [
+                'partners' => $company->getPartners()->map(fn($partner) => [
+                    'id' => $partner->getId(),
+                    'userName' => $partner->getUsername(),
+                    'email' => $partner->getEmail(),
+                ])->toArray()
+            ], Response::HTTP_OK
+        );
     }
 }
