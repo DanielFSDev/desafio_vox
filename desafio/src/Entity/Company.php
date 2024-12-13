b67758e92e06bae6bbc6b82d9272fb4dd260e189
daniel <danielfsilva.2019.2@gmail.com>
2024-12-12 22:09:02 -0300

    Criação da entidade company

--- desafio/src/Entity/Company.php
+++ desafio/src/Entity/Company.php
@@ -2,7 +2,97 @@
 
 namespace App\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity]
+#[ORM\Table(name: "company")]
 class Company
 {
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: "integer")]
+    private int $id;
 
+    #[ORM\Column(type: "string", length: 255)]
+    private string $name;
+
+    #[ORM\Column(type: "text")]
+    private string $address;
+
+    #[ORM\ManyToOne(targetEntity: User::class)]
+    #[ORM\JoinColumn(nullable: false, onDelete: "CASCADE")]
+    private User $owner;
+
+    #[ORM\ManyToMany(targetEntity: User::class, inversedBy: "companies")]
+    #[ORM\JoinTable(name: "company_user")]
+    private Collection $partners;
+
+    public function __construct()
+    {
+        $this->partners = new ArrayCollection();
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getAddress(): string
+    {
+        return $this->address;
+    }
+
+    public function setAddress(string $address): self
+    {
+        $this->address = $address;
+
+        return $this;
+    }
+
+    public function getOwner(): User
+    {
+        return $this->owner;
+    }
+
+    public function setOwner(User $owner): self
+    {
+        $this->owner = $owner;
+
+        return $this;
+    }
+
+    public function getPartners(): Collection
+    {
+        return $this->partners;
+    }
+
+    public function addPartner(User $partner): self
+    {
+        if (!$this->partners->contains($partner)) {
+            $this->partners->add($partner);
+        }
+
+        return $this;
+    }
+
+    public function removePartner(User $partner): self
+    {
+        $this->partners->removeElement($partner);
+
+        return $this;
+    }
 }
